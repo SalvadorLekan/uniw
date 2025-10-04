bdde826659a96d9b0f04193e9e8e51e1324e43f4
SalvadorLekan <lekansalvador@gmail.com>
2025-10-04 15:42:56 +0100

    chore: stuff

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,12 +1,5 @@
 import { Image } from "expo-image";
-import {
-  Platform,
-  Pressable,
-  StyleSheet,
-  Text,
-  View,
-  ViewBase,
-} from "react-native";
+import { Platform, Pressable, StyleSheet, Text, View } from "react-native";
 
 import { HelloWave } from "@/components/hello-wave";
 import ParallaxScrollView from "@/components/parallax-scroll-view";
@@ -19,21 +12,17 @@ export default function HomeScreen() {
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
-      headerImage={
-        <Image
-          source={require("@/assets/images/partial-react-logo.png")}
-          style={styles.reactLogo}
-        />
-      }
-    >
-      <View className="bg-amber-400  dark:bg-amber-900 lg:bg-blue-500">
-        <Text className="text-black-to-white">ggg</Text>
+      headerImage={<Image source={require("@/assets/images/partial-react-logo.png")} style={styles.reactLogo} />}>
+      <View className="bg-amber-400 dark:bg-amber-900 lg:bg-blue-500">
+        <Text className="text-black/10 text-var-blue-100 shadow-blue-600/10 shadow-[10px]">ggg</Text>
+      </View>
+      <View className="box">
+        <Text>hhh</Text>
       </View>
       <Pressable
         onPress={() => {
           Uniwind.setTheme(Uniwind.currentTheme === "dark" ? "light" : "dark");
-        }}
-      >
+        }}>
         <Text className="text-2">NNN</Text>
       </Pressable>
       <ThemedView style={styles.titleContainer}>
@@ -43,9 +32,7 @@ export default function HomeScreen() {
       <ThemedView style={styles.stepContainer}>
         <ThemedText type="subtitle">Step 1: Try it</ThemedText>
         <ThemedText>
-          Edit{" "}
-          <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText>{" "}
-          to see changes. Press{" "}
+          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes. Press{" "}
           <ThemedText type="defaultSemiBold">
             {Platform.select({
               ios: "cmd + d",
@@ -63,40 +50,22 @@ export default function HomeScreen() {
           </Link.Trigger>
           <Link.Preview />
           <Link.Menu>
-            <Link.MenuAction
-              title="Action"
-              icon="cube"
-              onPress={() => alert("Action pressed")}
-            />
-            <Link.MenuAction
-              title="Share"
-              icon="square.and.arrow.up"
-              onPress={() => alert("Share pressed")}
-            />
+            <Link.MenuAction title="Action" icon="cube" onPress={() => alert("Action pressed")} />
+            <Link.MenuAction title="Share" icon="square.and.arrow.up" onPress={() => alert("Share pressed")} />
             <Link.Menu title="More" icon="ellipsis">
-              <Link.MenuAction
-                title="Delete"
-                icon="trash"
-                destructive
-                onPress={() => alert("Delete pressed")}
-              />
+              <Link.MenuAction title="Delete" icon="trash" destructive onPress={() => alert("Delete pressed")} />
             </Link.Menu>
           </Link.Menu>
         </Link>
 
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
+        <ThemedText>{`Tap the Explore tab to learn more about what's included in this starter app.`}</ThemedText>
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
         <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
         <ThemedText>
           {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">
-            npm run reset-project
-          </ThemedText>{" "}
-          to get a fresh <ThemedText type="defaultSemiBold">app</ThemedText>{" "}
-          directory. This will move the current{" "}
+          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{" "}
+          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{" "}
           <ThemedText type="defaultSemiBold">app</ThemedText> to{" "}
           <ThemedText type="defaultSemiBold">app-example</ThemedText>.
         </ThemedText>
